8bea6df5c7aa66c9f7edc945fe9553189b569b45
SkeletalWaffles <acrystalflame@gmail.com>
2020-07-04 12:01:32 -0600

    Followed along with tutorial until I reached the 'Creating the Initial Login Component' video

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,46 @@
+import React, { Component, useReducer } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default class NavigationContainer extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='nav-wrapper'>
+                    <div className='left-side'>
+                        <div className='nav-link-wrapper'>
+                            <NavLink exact to='/' activeClassName='nav-link-active'>
+                                Home
+                            </NavLink>
+                        </div>
+
+                        <div className='nav-link-wrapper'>
+                            <NavLink to='/about-me' activeClassName='nav-link-active'>
+                                About
+                            </NavLink>
+                        </div>
+
+                        <div className='nav-link-wrapper'>
+                            <NavLink to='/contact' activeClassName='nav-link-active'>
+                                Contact
+                            </NavLink>
+                        </div>
+
+                        <div className='nav-link-wrapper'>
+                            <NavLink to='/blog' activeClassName='nav-link-active'>
+                                Blog
+                            </NavLink>
+                        </div>
+                    </div>
+
+                    <div className='right-side'>
+                        MARK FORSYTH
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
